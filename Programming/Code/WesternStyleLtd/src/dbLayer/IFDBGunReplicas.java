93a5d87210d3883799a6acab9e025936fcb667d9
IubiSoft <iubisoft.com@gmail.com>
2015-03-20 11:59:52 +0100

    Modified interfaces !

--- Programming/Code/WesternStyleLtd/src/dbLayer/IFDBGunReplicas.java
+++ Programming/Code/WesternStyleLtd/src/dbLayer/IFDBGunReplicas.java
@@ -7,6 +7,6 @@ import modelLayer.*;
 public interface IFDBGunReplicas {
 	  public ArrayList<GunReplicas> getGunReplicas(boolean retriveAssociation);
 	   public GunReplicas findGunReplicas(int id, boolean retriveAssosciation);
-	   public int insert(GunReplicas dept);
+	   public int insert(GunReplicas dept) throws Exception;
 	   public int update(GunReplicas dept);
 }
--- Programming/Code/WesternStyleLtd/src/dbLayer/IFDBInvoice.java
+++ Programming/Code/WesternStyleLtd/src/dbLayer/IFDBInvoice.java
@@ -7,6 +7,6 @@ import modelLayer.*;
 public interface IFDBInvoice {
 	   public ArrayList<Invoice> getAllInvoice(boolean retriveAssociation);
 	   public Invoice findInvoice(int saleId, boolean retriveAssosciation);
-	   public int insert(Invoice dept);
+	   public int insert(Invoice dept) throws Exception;
 	   public int update(Invoice dept);
 }
